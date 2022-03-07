f26baa9a3d7c68ff5e6b9b0baf4d49e93f191d0e
Mayank Vishnoi <mayankvishnoi@Mayanks-MacBook-Air.local>
2022-03-07 23:12:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DumbComponent from './dumb/DumbComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <DumbComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dumb/DumbComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DumbComponent (){
+    return (
+        <div>Hey Dumb</div>
+    )
+}
