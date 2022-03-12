89fa553a01cce3d65a76a50662f449ddca6f870f
Mayank Vishnoi <mayankvishnoi@Mayanks-MacBook-Air.local>
2022-03-12 22:58:01 +0530

    Responsive UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import DumbContainer from './dumb/DumbContainer';
-import DumbHeaderComponent from './dumb/header/DumbHeaderComponent';
+import "./App.css";
+// import DumbContainer from './dumb/DumbContainer';
+import DumbHeaderComponent from "./dumb/header/DumbHeaderComponent";
+import DumbHomeComponent from "./dumb/home/DumbHomeComponent";
 
 function App() {
   return (
     <div className="App">
-      <DumbContainer/>
+      <DumbHomeComponent />
     </div>
   );
 }
--- src/dumb/home/DumbHomeComponent.js
+++ src/dumb/home/DumbHomeComponent.js
@@ -1,36 +1,42 @@
 import React from "react";
 import "./DumbHomeComponent.css";
+import Header from "../header/DumbHeaderComponent";
+import Footer from "../footer/Footer";
 import dumbIllustration from "../../assets/images/dumb0_5.png";
 
 export default function DumbHomeComponent() {
   return (
     <>
-      <div className="home-container">
-        <div className="assessment-info">
-          <div className="assessment-heading">
-            Dumb <br /> Assessment
-          </div>
-          <div className="assessment-description">
-            <p>
-              Usually in our life, our beloved Friends and families calls us
-              Dumb based on our actions, behaviour and choices of selection.
-              <br />
-              This assessment will help you to figure out once in for all, that
-              if you really are <span> DUMB</span> or <span>NOT</span> .
-            </p>
-          </div>
-          <div className="assessment-option">
-            Please Enter you name below and click on Start:
-            <div className="assessment-input">
-              <input type="text" />
-              <button type="submit">Start</button>
+      <div className="main-container">
+        <Header />
+        <div className="home-container">
+          <div className="assessment-info">
+            <div className="assessment-heading">
+              Dumb <br /> Assessment
+            </div>
+            <div className="assessment-description">
+              <p>
+                Usually in our life, our beloved Friends and families calls us
+                Dumb based on our actions, behaviour and choices of selection.
+                <br />
+                This assessment will help you to figure out once in for all,
+                that if you really are <span> DUMB</span> or <span>NOT</span> .
+              </p>
+            </div>
+            <div className="assessment-option">
+              Please Enter you name below and click on Start:
+              <div className="assessment-input">
+                <input type="text" />
+                <button type="submit">Start</button>
+              </div>
             </div>
           </div>
+          <div className="illustration-icon">
+            {/* <div className="illustration-icon-background"></div> */}
+            <img src={dumbIllustration} />
+          </div>
         </div>
-        <div className="illustration-icon">
-          {/* <div className="illustration-icon-background"></div> */}
-          <img src={dumbIllustration} />
-        </div>
+        {/* <Footer /> */}
       </div>
     </>
   );
